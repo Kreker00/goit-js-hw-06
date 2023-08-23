00f1419af1a93ad2d706967e6e781108a9e8a63f
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-08-23 22:52:54 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+// function nameL(arr) {
+//     const li = arr.map((liName) => `<li class='item'>${liName}</li>`).join("");
+//     list.insertAdjacentHTML("beforeend", li);
+// }
+// nameL(ingredients);
+const list = document.querySelector("#ingredients");
+const listElemants = ingredients.map((element) => {
+    const li = document.createElement("li");
+    li.classList.add("item");
+    li.textContent = element;
+    return li;
+});
+
+list.append(...listElemants);
