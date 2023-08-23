701c55e18e68a4ef93f936892ad4b570c5ff884e
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-08-23 16:51:39 +0300

    task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,5 +1,7 @@
-const categoriesLength = [];
-const categories = (categorie) => {
-    #categories 
-};
-// namberCategories;
+const ulCategories = document.querySelectorAll(`.item`);
+console.log(`Number of categories: ${ulCategories.length}`);
+
+ulCategories.forEach(function (list) {
+    console.log(`Category: ${list.firstElementChild.textContent}`);
+    console.dir(`Elements: ${list.lastElementChild.children.length}`);
+});
