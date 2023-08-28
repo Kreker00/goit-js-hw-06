9c2528010688df563a2c4e46b31a18ec270f2049
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-08-28 16:22:45 +0300

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+
+button.addEventListener("click", onClickButton);
+
+function onClickButton() {
+    body.style.backgroundColor = getRandomHexColor();
 }
