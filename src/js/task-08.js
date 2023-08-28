675975efd273d30951ef3b75aa98ec6f672702ff
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-08-28 15:59:22 +0300

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+
+    if (email.value === "" && password.value === "") {
+        alert("There are blank fields in the form!");
+    }
+    console.log({ Email: email.value, Password: password.value });
+    event.currentTarget.reset();
+}
